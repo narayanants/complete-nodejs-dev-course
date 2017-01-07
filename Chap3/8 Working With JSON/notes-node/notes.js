7a840118505d87b4b4a97ee2b5ab1875bbd388a9
Narayanan Ts <tsnarayanan7@gmail.com>
2017-01-07 13:27:46 +0530

    Working with JSON

---
+++ Chap3/8 Working With JSON/notes-node/notes.js
@@ -0,0 +1,26 @@
+console.log('Starting notes.js');
+
+var addNote = (title,body)=>{
+    console.log('Adding Note',title,body);
+};
+
+var getAll = ()=>{
+    console.log('Listing all notes');
+};
+
+var getNote = (title)=>{
+    console.log('Reading note',title);
+}
+
+var removeNote = (title)=>{
+    console.log('Removing note',title);
+};
+
+
+module.exports = {
+    //addNote:addNote similar to below
+    addNote,
+    getAll,
+    getNote,
+    removeNote
+};
