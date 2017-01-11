23cf3e2398cd0854158f85d5cf85b6b07ec6d51e
Narayanan Ts <tsnarayanan7@gmail.com>
2017-01-11 10:51:30 +0530

    Recap of notes.js

--- Chap3/16 Requiring Arguments and Advanced Yargs/notes-node/notes1.js
+++ Chap3/16 Requiring Arguments and Advanced Yargs/notes-node/notes1.js
@@ -1,67 +1,61 @@
-console.log('Starting notes.js');
+const fs = require('fs');
 
-const Promise = require('bluebird');
-const fs = Promise.promisifyAll(require('fs'));
-
-var fetchNotes = ()=>{
-    try{
-        var noteString = fs.readFileSync('notes-data.json');
-        return JSON.parse(noteString);
-    }catch(e){  
-        return [];
-    }
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch (e) {
+    return [];
+  }
 };
 
-var saveNotes = (notes)=>{
-    fs.writeFileSync('notes-data.json',JSON.stringify(notes));
+var saveNotes = (notes) => {
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
 };
 
-var addNote = (title,body)=>{
-    var notes = fetchNotes(),
-        note = {
-            title,
-            body
-        },
-    var duplicateNotes = notes.filter((note)=>note.title === title);
-    if(duplicateNotes.length === 0 ){
-        notes.push(note);
-        saveNotes(notes);
-        return note;
-    }
+var addNote = (title, body) => {
+  var notes = fetchNotes();
+  var note = {
+    title,
+    body
+  };
+  var duplicateNotes = notes.filter((note) => note.title === title);
 
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    saveNotes(notes);
+    return note;
+  }
 };
 
-var getAll = ()=>{
-    console.log('Getting all users');
+var getAll = () => {
+  return fetchNotes();
 };
 
-var getNote = (title)=>{
-    var notes = fetchNotes(),
-        filteredNotes = notes.filter((note)=>note.title ==== title);
-        return filteredNotes[0];
+var getNote = (title) => {
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter((note) => note.title === title);
+  return filteredNotes[0];
 };
 
-var removeNote = (title)=>{
-    var notes = fetchNotes(),
-        filteredNotes = notes.filter((note)=>note.title !== title);
-        saveNotes(filteredNotes);
+var removeNote = (title) => {
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter((note) => note.title !== title);
+  saveNotes(filteredNotes);
 
-    return notes.length !== filteredNotes.length;
+  return notes.length !== filteredNotes.length;
 };
 
-
-var logNote = (note)=>{
-    console.log('------------');
-    console.log(`Title ${note.title}`);
-    console.log(`Body ${note.body}`);
+var logNote = (note) => {
+  console.log('--');
+  console.log(`Title: ${note.title}`);
+  console.log(`Body: ${note.body}`);
 };
 
 module.exports = {
-    addNote,
-    removeNote,
-    getAll,
-    getNote,
-    logNote
+  addNote,
+  getAll,
+  getNote,
+  removeNote,
+  logNote
 };
-
-
