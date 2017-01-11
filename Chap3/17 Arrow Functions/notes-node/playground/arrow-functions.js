b3284c972bb780da3eb4d400b5536a471266652d
Narayanan Ts <tsnarayanan7@gmail.com>
2017-01-11 11:25:18 +0530

    arrow functions

---
+++ Chap3/17 Arrow Functions/notes-node/playground/arrow-functions.js
@@ -0,0 +1,30 @@
+var square = (x) => x * x;
+console.log(square(9));
+
+var user = {
+    name : 'Andrew',
+    sayHi : ()=>{
+        console.log(arguments);
+        console.log(`Hi. I am ${this.name}`);
+    },
+    sayHiAlt (){
+        console.log(arguments);
+        console.log(`Hi. I am ${this.name}`);
+    }
+};
+
+user.sayHi(1,2,3);
+
+
+
+
+/*
+    Arrow functions:
+    =================
+
+        * Don't give access to this binding
+        * No arguments keywords
+
+ */
+
+
